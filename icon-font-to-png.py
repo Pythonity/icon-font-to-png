2cce5bbe44f599f4af0d8abdebf66048e1bc17dd
Michal Wojciechowski <odyniec@odyniec.net>
2014-07-05 09:09:41 +0200

    Remove parentheses

--- icon-font-to-png.py
+++ icon-font-to-png.py
@@ -67,7 +67,7 @@ def load_css(filename, strip_prefix):
         for name in new_icons.keys():
             new_icons[name[len(common_prefix):]] = new_icons.pop(name)
     
-    return (new_icons, common_prefix)
+    return new_icons, common_prefix
 
 def export_icon(icon, size, filename, ttf_file, color):
     image = Image.new("RGBA", (size, size), color=(0,0,0,0))
@@ -140,7 +140,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    (icons, common_prefix) = load_css(args.css_file, not args.keep_prefix)
+    icons, common_prefix = load_css(args.css_file, not args.keep_prefix)
 
     if args.list:
         for icon in sorted(icons.keys()):

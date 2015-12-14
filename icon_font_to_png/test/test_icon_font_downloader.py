e1442576b47f1c8e8fa284a76e59273ed82882cf
Paweł Adamczak <pawel.ad@gmail.com>
2015-12-14 17:33:37 +0100

    Added latest version number test

--- icon_font_to_png/test/test_icon_font_downloader.py
+++ icon_font_to_png/test/test_icon_font_downloader.py
@@ -4,40 +4,54 @@ import pytest
 import os
 import tempfile
 
-from icon_font_to_png import icon_font_downloader
+from icon_font_to_png.icon_font_downloader import (
+    FontAwesomeDownloader, OcticonsDownloader
+)
 
 
 # Font Awesome
 def test_font_awesome():
     """Test initializing Font Awesome Downloader"""
     # With directory
-    obj = icon_font_downloader.FontAwesomeDownloader(tempfile.mkdtemp())
+    obj = FontAwesomeDownloader(tempfile.mkdtemp())
     obj.download_files()
 
     assert os.path.isfile(obj.css_path)
     assert os.path.isfile(obj.ttf_path)
 
     # Without directory
-    obj = icon_font_downloader.FontAwesomeDownloader()
+    obj = FontAwesomeDownloader()
     obj.download_files()
 
     assert os.path.isfile(obj.css_path)
     assert os.path.isfile(obj.ttf_path)
+
+
+def test_font_awesome_latest_version_number():
+    """Test that getting latest version number"""
+    obj = FontAwesomeDownloader(tempfile.mkdtemp())
+    assert obj.get_latest_version_number()
 
 
 # Octicons
 def test_octicons():
     """Test initializing Octicons Downloader"""
     # With directory
-    obj = icon_font_downloader.OcticonsDownloader(tempfile.mkdtemp())
+    obj = OcticonsDownloader(tempfile.mkdtemp())
     obj.download_files()
 
     assert os.path.isfile(obj.css_path)
     assert os.path.isfile(obj.ttf_path)
 
     # Without directory
-    obj = icon_font_downloader.OcticonsDownloader()
+    obj = OcticonsDownloader()
     obj.download_files()
 
     assert os.path.isfile(obj.css_path)
     assert os.path.isfile(obj.ttf_path)
+
+
+def test_octicons_latest_version_number():
+    """Test that getting latest version number"""
+    obj = OcticonsDownloader(tempfile.mkdtemp())
+    assert obj.get_latest_version_number()

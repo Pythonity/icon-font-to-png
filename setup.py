37f222d7c436aebedbc5f2a3eca678fd924cdea0
Paweł Adamczak <pawel.ad@gmail.com>
2017-01-20 20:17:12 +0000

    Updated requirements

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'Pillow>=4.0.0',
-        'requests>=2.12.4',
+        'requests>=2.12.5',
         'six>=1.10.0',
         'tinycss>=0.4',
     ],
